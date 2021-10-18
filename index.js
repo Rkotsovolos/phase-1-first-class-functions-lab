480278fc38d8cfbd79051c42733e10c8afdf2082
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-18 16:01:25 -0700

    commit tree 19498da8d02b0ffc92c7cb677bf169f1e27e8e3c

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = () => {
+    let newArray = drivers.slice(0,2);
+    return newArray;
+}
+
+const returnLastTwoDrivers = () => {
+    let newArray2 = drivers.slice(2);
+    return newArray2;
+}
+console.log(returnFirstTwoDrivers())
+console.log(returnLastTwoDrivers())
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function callBackNumber() {
+    return 
+}
+
+
+function createFareMultiplier(multiplier) {    
+    // const fareDoubler = (someFare) => someFare * someNumber
+     return function(theFare) {
+        return theFare * multiplier;
+     }
+
+}
+
+
+ const fareDoubler = createFareMultiplier(2)
+
+// fareDoubler(4);
+// createFareMultiplier(2)(4)
+
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers();
+}
